3fddb04cd3166cb8d91d924bd58bffaa261e7014
AmirBrDr <amirmahdi.ghasemi002@gmail.com>
2023-03-28 08:35:41 +0200

    creation de la classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,43 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boisson;
+	private int argent;
+	public Humain(String nom, String boisson, int argent) {
+		this.nom = nom;
+		this.boisson = boisson;
+		this.argent = argent;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public int getArgent() {
+		return argent;
+	}
+	private void parler(String texte) {
+		System.out.println(texte);
+	}
+	public void direBonjour() {
+		parler("Bonjour! Je m'appelle "+getNom()+" et J'aime boire du "+this.boisson);
+	}
+	public void boire(String boisson) {
+		parler("Mmmm, un bon verre de "+boisson+"GLOUPS!");
+	}
+	public void acheter(String bien,int prix) {
+		if(this.argent>=prix) {
+			perdreArgent(prix);
+			parler("J'ai acheter un "+bien+" qui coute "+prix);
+		}
+		else {
+			parler("Je n'ai pas assez d'argent");
+		}
+	}
+	private void perdreArgent(int montant) {
+		this.argent-=montant;
+	}
+	public void gagnerArgent(int montant) {
+		this.argent+=montant;
+	}
+	
+}
